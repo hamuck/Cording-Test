38004352852c792ffbdd148755eb9b7c567c400d
yejin <97212969+hamuck@users.noreply.github.com>
2024-10-28 10:29:49 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.09 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -6,14 +6,12 @@ class Solution {
             if (num % 2 == 0 && count < 500){
                 num = num/2 ;
                 count++;
-                System.out.println(count+"와"+num);
             }else if(count >= 500){
                 count = -1;
                 break;
             }else{
                 num = num*3+1;
                 count++;
-                System.out.println(count+"와"+num);
             }
         }
         
