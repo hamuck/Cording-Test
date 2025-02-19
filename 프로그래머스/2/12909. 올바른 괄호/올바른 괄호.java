670520a59d06db13162567859926651a0818c6f9
yejin <97212969+hamuck@users.noreply.github.com>
2025-02-19 20:15:11 +0900

    [level 2] Title: 올바른 괄호, Time: 47.68 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        if(s.charAt(0) == '(' && s.charAt(s.length() -1) == ')'){
+            int cnt = 0;
+            String arr[] = s.split("");
+            
+            for(String i : arr){
+                if(i.equals("(")){
+                    cnt++;
+                }else{
+                    cnt--;
+                }
+                if (cnt < 0) {
+                    return false;
+                }
+            }
+           return cnt == 0;
+        }
+
+        return false;
+    }
+}
