14ebf13d4c0c129f2e4e97cede4f3e1ad6b78420
yejin <97212969+hamuck@users.noreply.github.com>
2025-03-03 20:17:07 +0900

    [level 2] Title: N개의 최소공배수, Time: 47.73 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,6 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] arr) {
-        int answer = 0;
-        return answer;
+        Arrays.sort(arr);
+        
+        int max = arr[arr.length - 1];
+        int min = arr[0];
+        int multiple = max * min;
+
+        while (true) {
+            boolean isTrue = true;
+            for (int num : arr) {
+                if (multiple % num != 0) {
+                    isTrue = false;
+                    break;
+                }
+            }
+            if (isTrue) {
+                return multiple;
+            }
+            multiple++;
+        }
     }
 }
