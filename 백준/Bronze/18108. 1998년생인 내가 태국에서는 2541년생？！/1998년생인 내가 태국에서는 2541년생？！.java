4fde9211a3190219e54d5c9d02e9ea6c0a33af0b
yejin <97212969+hamuck@users.noreply.github.com>
2025-03-21 09:34:30 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        int a = 2541 - 1998;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.print(num - a);
+    }
+}
